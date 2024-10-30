05f6463cefa4f43c74f10b123f118ad46d04a0ce
MusingRabbit <scramuk@gmail.com>
2024-10-30 13:31:22 +0000

    Updated player movement script

--- Tweed/Assets/Scripts/PlayerMovement.cs
+++ Tweed/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[SerializeField] private Rigidbody PlayerBody;
-
-
     //Player Movement speed
-    public static float speed = 5f;
-    public static float rotationSpeed = 700f;
+    [SerializeField]
+    private float speed = 5f;
 
-    [SerializeField] private Transform CamDir;
+    [SerializeField]
+    private float rotationSpeed = 700f;
+
+    private Vector3 m_velocity;
+
+    private Rigidbody m_rigidBody;
+
+    [SerializeField] 
+    private Transform CamDir;
     //private Vector3 PlayerMovementInput;
 
-    
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        m_velocity = Vector3.zero;
+
+        m_rigidBody = GetComponent<Rigidbody>();
     }
 
    
@@ -39,12 +47,18 @@ public class PlayerMovement : MonoBehaviour
         PlayerMovementInput = Quaternion.AngleAxis(CamDir.rotation.eulerAngles.y, Vector3.up) * PlayerMovementInput;
         PlayerMovementInput.Normalize();
 
-        transform.Translate(PlayerMovementInput * speed * Time.deltaTime, Space.World);
+        m_velocity += PlayerMovementInput * (speed * Time.deltaTime);
+
+        this.transform.position += m_velocity;
+
+        m_velocity -= m_velocity * 0.25f;
+
+        Debug.Log(m_velocity);
+        //transform.Translate(PlayerMovementInput * speed * Time.deltaTime, Space.World);
 
         if (PlayerMovementInput != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(PlayerMovementInput, Vector3.up);
-
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
         //MovePlayer();
