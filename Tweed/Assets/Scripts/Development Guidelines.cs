f713f6cac15fd679604598cad61ae191ab63c5be
HomelyZacombie <91287052+HomelyZacombie@users.noreply.github.com>
2024-11-16 15:16:16 +0000

    Development Guildlines
    Added a script that's to be used as a guide line to our projects development

--- Tweed/Assets/Scripts/Development Guidelines.cs
+++ Tweed/Assets/Scripts/Development Guidelines.cs
@@ -5,39 +5,77 @@ using UnityEngine;
 /*public class DevelopmentGuidelines : MonoBehaviour
 {
    
-
+----------------
 ---------------- Bottem line requirements functions and mechanics
+----------------
 
 Player movement (Done) <-- needs polish on run responcivness
 Respawn of player (Not started) <-- When caught dies and respawns.
+Player interaction (Done) <-- works as intended.
 
-Soldier AI behavour towords player (Implamented) <-- needs damage function on player to kill
+Bot/Xeno animations (Done) <-- works as intended.
+Andy modal (Not started) <-- modal for the goal of the game.
+Gates (Done) <-- bug of one door opens fully the other just short.
+
+Bot/Xeno AI behavour towords player (Done) <-- needs damage function on player to kill
 
 Main Menu (early implamentation) <-- Game starts at main menu main menu.
-Win state (early implamentation) <--  you win Image shows then goes to main menu.
+Win state (Not started) <--  you win Image shows then goes to main menu.
 Pause Menu (not started) <-- when player hits Esc game pauses
 
-
+--------
 -------- Bottem line Map related
+--------
 
 Built map (Implamented) <-- need to edit to make paths wider with larger sections to allow avoidence of AI possible
 Gate Progression placement <think 3 needed> (Not started) <-- spots on the map need to be chosen for 'Gate' placement to act as Progression indicaters and barriers to the player.
 
+Bot/Xeno patrol point placement (Not Started) <-- waiting on map laoyout adit
+
+Ambiant music (Not Started) <-- Background music for main menu and game scene
 
 
+
+----------------
 ---------------- Low End streatch goals
+----------------
 
 Player Multiple respawn points (Not started) <-- as gates are opened respawns for player are added to array and chosen at random when respawning.
 
 Platform Walker Animations (implamented) <-- Might change depending on code implmentation
 Platform Walker Pathing (not started) <-- Platform Walker when activated by button(control panel) will travel from one point to the other carrying the player across hazerd.
+Neutral Bot Animations (Done) <-- yet to be tested live
+Bot friendly animations (Done) <-- yet to be tested live
 
+Neutral Bot interactions/speach bubble (Not started) <-- player can interact and free Neutral bots and speach bubbles will appear from them.
+Bot converstion (Not started) <-- when player interacts with spisific control panels some Bot's become friendly and will ignore player
+
+Main menu live background (Not started) <-- live background showing small elements of the game
+
+--------
 -------- Low End Map related
+--------
 
 lake barrier (Not started) <-- A section that is below sea level to act as impassible terrain for our platform walker to be used. As a goal/puzzle to progress
 
------------------ High End stretch goals
+Rotating sky box (Not started) <-- To make the world feel more alive
 
+
+
+
+----------------
+---------------- High End stretch goals
+----------------
+
+
+
+
+
+--------
 -------- High End Map related
+--------
+
+
+
 
 }*/
